2184b5921d88e3e8f5fc403596314a534514f8ac
ShayG0 <86621168+ShayG0@users.noreply.github.com>
2022-02-09 18:49:30 +0200

    add activities

--- app/src/main/java/com/example/androidproject/AddTaskActivity.java
+++
@@ -1,2 +0,0 @@
-package com.example.androidproject;public class AddTaskActivity {
-}
--- app/src/main/java/com/example/androidproject/NewTaskActivity.java
+++ app/src/main/java/com/example/androidproject/NewTaskActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TasksActivity extends AppCompatActivity {
+public class NewTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tasks);
+        setContentView(R.layout.activity_new_task);
     }
 }
